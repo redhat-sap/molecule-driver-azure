edb66587a61733cb53cb058dfba6d7858b1b2b1d
kksat <>
2022-09-16 10:33:52 +0200

    add publish to pypi.org

--- setup.py
+++ setup.py
@@ -3,4 +3,6 @@ import setuptools
 
 
 if __name__ == "__main__":
-    setuptools.setup(use_scm_version=True, setup_requires=["setuptools_scm"])
+    # setuptools.setup(use_scm_version=False, setup_requires=["setuptools_scm"])
+    # setuptools.setup(version="0.0.2")
+    setuptools.setup()
